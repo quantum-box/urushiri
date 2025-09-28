d2d90a2baa7830b4699b97f4be87b1a5a4158d61
miorin1224 <miori.kawamura@be-cause.co.jp>
2025-09-28 14:24:00 +0900

    修正

--- types/participant.ts
+++ types/participant.ts
@@ -3,12 +3,3 @@ export type AgeGroup = "teens" | "twenties" | "thirties" | "forties" | "fifties"
 export type OccupationCategory = "student" | "engineer" | "designer" | "planner" | "manager" | "other"
 
 export type DiscoverySource = "sns" | "search" | "friend" | "media" | "eventSite" | "other"
-
-export interface EventParticipant {
-  id: string
-  ageGroup: AgeGroup
-  occupation: OccupationCategory
-  discovery: DiscoverySource
-  sharedEventTitles: string[]
-  other?: string
-}
